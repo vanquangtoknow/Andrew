1217b8c5a89c62668e385b81da14517e90c2ac8c
kduy969 <kduyit92@gmail.com>
2013-12-09 23:01:01 +0700

    1

---
+++ NavigationDrawer v1/src/DTO/CheckTable.java
@@ -0,0 +1,59 @@
+package DTO;
+
+import org.ksoap2.serialization.SoapObject;
+
+public class CheckTable {
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public int getId_Employee() {
+		return Id_Employee;
+	}
+	public void setId_Employee(int id_Employee) {
+		Id_Employee = id_Employee;
+	}
+	public boolean isCheck_Status() {
+		return Check_Status;
+	}
+	public void setCheck_Status(boolean check_Status) {
+		Check_Status = check_Status;
+	}
+	public String getCheckIn() {
+		return CheckIn;
+	}
+	public void setCheckIn(String checkIn) {
+		CheckIn = checkIn;
+	}
+	public String getCheckOut() {
+		return CheckOut;
+	}
+	public void setCheckOut(String checkOut) {
+		CheckOut = checkOut;
+	}
+	private int Id ;
+	private int Id_Employee ;
+	private boolean Check_Status ;
+	private String CheckIn ;
+	private String CheckOut ;
+	private SoapObject soap;
+	
+	public SoapObject getSoap() {
+		return soap;
+	}
+	public void setSoap(SoapObject soap) {
+		this.soap = soap;
+	}
+	public void getInfoFromSoap(SoapObject root)
+    {
+		this.soap = root;
+    	this.setCheckIn((root.getProperty(0).toString()));
+    	this.setCheckOut((root.getProperty(1).toString()));
+    	this.setCheck_Status(Boolean.parseBoolean(root.getProperty(2).toString()));
+		this.setId(Integer.parseInt(root.getProperty(3).toString()));
+		this.setId_Employee(Integer.parseInt(root.getProperty(4).toString()));
+		
+    }
+}
